b95894aeb01e1439d99738585307309c47f27edb
Jeff Saltzman <jmsaltzman@gmail.com>
2015-06-09 12:24:04 -0700

    Arduino setup commands

--- arduino_setup.sh
+++ arduino_setup.sh
@@ -4,7 +4,7 @@
 
 # from http://razzpisampler.oreilly.com/ch10.html
 
-cd ~/Downloads
+#cd ~/Downloads
 apt-get update
 apt-get install -y arduino
 wget https://github.com/wyolum/alamode/blob/master/bundles/alamode-setup.tar.gz?raw=true -O alamode-setup.tar.gz
