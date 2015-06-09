c94848f816e50cfa6258d4a976476d86163f1a33
Jeff Saltzman <jmsaltzman@gmail.com>
2015-06-09 11:43:32 -0700

    initial apt-get commands

---
+++ raspi_install1.sh
@@ -0,0 +1,15 @@
+#! /bin/csh -f
+
+# execute w/sudo
+
+apt-get update
+apt-get upgrade
+
+apt-get install -y git
+# git book:
+# https://git-scm.com/book/en/v2
+
+apt-get install -y tightvncserver
+# details on tightvncserver setup:
+# http://www.howtogeek.com/141157/how-to-configure-your-raspberry-pi-for-remote-shell-desktop-and-file-transfer/all/
+
