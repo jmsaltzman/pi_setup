a9f00b87f0260f9ee02581b69d8d4ebfccca6c5e
Jeff Saltzman <jmsaltzman@gmail.com>
2015-06-09 10:58:01 -0700

    now with Arduino .sh setup
    sh file for Arduino setup, from:
    http://razzpisampler.oreilly.com/ch10.html

---
+++ arduino_setup.sh
@@ -0,0 +1,15 @@
+#! /bin/csh -f
+
+# execute w/sudo
+
+# from http://razzpisampler.oreilly.com/ch10.html
+
+cd ~/Downloads
+apt-get update
+apt-get install -y arduino
+wget https://github.com/wyolum/alamode/blob/master/bundles/alamode-setup.tar.gz?raw=true -O alamode-setup.tar.gz
+tar -xvzf alamode-setup.tar.gz
+cd alamode-setup
+./setup
+
+# reboot for changes to take effect
