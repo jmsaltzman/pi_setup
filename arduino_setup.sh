3476712592b289fa0993a15010cacb95af30f5fc
Jeff Saltzman <jmsaltzman@gmail.com>
2015-06-10 12:27:35 -0700

    Remove install archive and setup directory

--- arduino_setup.sh
+++ arduino_setup.sh
@@ -2,12 +2,15 @@
 
 # from http://razzpisampler.oreilly.com/ch10.html
 
-#cd ~/Downloads
 sudo apt-get update
 sudo apt-get install -y arduino
 wget https://github.com/wyolum/alamode/blob/master/bundles/alamode-setup.tar.gz?raw=true -O alamode-setup.tar.gz
 tar -xvzf alamode-setup.tar.gz
 cd alamode-setup
 sudo ./setup
+
+cd ~/pi_setup
+rm -rf alamode-setup
+rm alamode-setup.tar.gz
 
 # reboot for changes to take effect
