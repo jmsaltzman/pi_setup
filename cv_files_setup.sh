6bbf156f575605d2c4ba5270f40b16dc0b0035d2
Jeff Saltzman <jmsaltzman@gmail.com>
2015-06-10 11:48:54 -0700

    Mods to correctly unarchive test files

--- cv_files_setup.sh
+++ cv_files_setup.sh
@@ -1,13 +1,6 @@
 #!/bin/sh -f
 
-export sipi_aerials="http://sipi.usc.edu/database/aerials.tar.gz"
-export sipi_misc="http://sipi.usc.edu/database/misc.tar.gz"
-export sipi_tx="http://sipi.usc.edu/database/textures.tar.gz"
-export sipi_seq="http://sipi.usc.edu/database/sequences.tar.gz"
-export ipp_standard="http://imageprocessingplace.com/downloads_V3/root_downloads/image_databases/standard_test_images.zip"
-echo $sipi_aerials
-
-: <<'END'
+# Create directory tree
 cd
 mkdir book
 cd book
@@ -26,21 +19,32 @@ mkdir chapter07
 mkdir chapter08
 mkdir chapter09
 mkdir chapter10
-END
 
-echo "after comment block..."
-#cd ../test_set
+# Get and unarchive test files
+cd ../test_set
+
+export sipi_aerials="http://sipi.usc.edu/database/aerials.tar.gz"
+export sipi_misc="http://sipi.usc.edu/database/misc.tar.gz"
+export sipi_tx="http://sipi.usc.edu/database/textures.tar.gz"
+export sipi_seq="http://sipi.usc.edu/database/sequences.tar.gz"
+export ipp_standard="http://imageprocessingplace.com/downloads_V3/root_downloads/image_databases/standard_test_images.zip"
 
 wget $sipi_aerials
+wget $sipi_misc
+wget $sipi_tx
+wget $sipi_seq
+wget $ipp_standard
+
 tar -xvf `basename $sipi_aerials`
+tar -xvf `basename $sipi_misc`
+tar -xvf `basename $sipi_tx`
+tar -xvf `basename $sipi_seq`
+unzip `basename $ipp_standard`
 
-#wget $sipi_misc
-#tar -xvf $sipi_misc
-#wget $sipi_tx
-#tar -xvf $sipi_tx
-#wget $sipi_seq
-#tar -xvf $sipi_seq
-#wget $ipp_standard
-#unzip $ipp_standard
+#rm `basename $sipi_aerials`
+rm `basename $sipi_misc`
+#rm `basename $sipi_tx`
+#rm `basename $sipi_seq`
+#rm `basename $ipp_standard`
 
-
+echo "Done retrieving and unarchiving image files."
