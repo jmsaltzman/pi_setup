a435509e332fc25ddb128efcfc9689421d4bced2
Jeff Saltzman <jmsaltzman@gmail.com>
2015-06-10 12:16:22 -0700

    smoother bashrc bashing

--- bash_setup.sh
+++ bash_setup.sh
@@ -3,4 +3,5 @@
 # Get aliases and set prompt
 mv .bash_aliases ~/
 cp ~/.bashrc ~/.bashrc_old
-cat ~/.bashrc .bashrc_mods > ~.bashrc
+cat ~/.bashrc .bashrc_mods > .bashrc_new
+mv .bashrc_new ~/.bashrc
