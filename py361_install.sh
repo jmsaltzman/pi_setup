f7a8923ebc017db02dfb8a83c77d36f96973f23c
Jeff Saltzman <jmsaltzman@gmail.com>
2017-06-27 12:24:34 -0500

    update to python installer

--- py361_install.sh
+++ py361_install.sh
@@ -3,10 +3,9 @@
 # instructions for installing Python 3.6.1
 # Mostly from https://gist.github.com/dschep/24aa61672a2092246eaca2824400d37f
 
-
-# Install required build tools; these may already be installed
-sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev \ 
-libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+# Install required build tools; some may already be installed
+sudo apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev \
+  libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 
 # Download, uncompress, build, and install Python
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
@@ -19,10 +18,14 @@ sudo make altinstall
 # Remove installation files
 sudo rm -r Python-3.6.0
 rm Python-3.6.0.tgz
-#sudo apt-get --purge remove build-essential tk-dev
-#sudo apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
-#sudo apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
-#sudo apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+
+# Remove build tools
+sudo apt-get -y --purge remove build-essential tk-dev
+sudo apt-get -y --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev
+sudo apt-get -y --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+sudo apt-get -y --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+
+# Clean up
 sudo apt-get autoremove
 sudo apt-get clean
 
