2f75a13610de528f3ff07f8534786a9f551a76c0
Jeff Saltzman <jmsaltzman@gmail.com>
2017-10-26 16:18:03 -0500

    Made version a variable in py36_install.sh

--- py36_install.sh
+++ py36_install.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-# instructions for installing Python 3.6.2
+# instructions for installing Python 3.6.x
 # Mostly from: https://gist.github.com/dschep/24aa61672a2092246eaca2824400d37f
 # Simpler: https://raspberrypi.stackexchange.com/questions/59381/how-do-i-update-my-rpi3-to-python-3-6
 
 # Issues:
-# "python3" still runs 3.4.2, not 3.6.2
+# "python3" still runs 3.4.2, not 3.6.x!
 
-echo "Starting Python 3.6.2 installation, this will take a while..."
+# Set the version
+ver="3.6.1"
+
+echo "Starting Python $ver installation, this will take a while..."
 
 # Download, uncompress, build, and install Python
-wget https://www.python.org/ftp/python/3.6.3/Python-3.6.2.tgz
-sudo tar xzvf Python-3.6.2.tgz
-cd Python-3.6.2
-./configure
+wget https://www.python.org/ftp/python/$ver/Python-$ver.tgz
+
+sudo tar xzvf Python-$ver.tgz
+cd Python-$ver
+sudo ./configure
 sudo make
 sudo make install
 
@@ -28,4 +32,3 @@ then
 else
     echo "NOT removing installation files"
 fi
-
