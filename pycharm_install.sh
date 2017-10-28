4d4e07a2076fb99262ce66bf2fc781cab6ba6671
Jeff Saltzman <jmsaltzman@gmail.com>
2017-10-27 22:42:33 -0500

    Tested pycharm installer script

--- pycharm_install.sh
+++ pycharm_install.sh
@@ -3,13 +3,11 @@
 # Installation instructions:
 # https://www.jetbrains.com/help/pycharm/installation-and-launching.html#linux
 
-echo "PyCharm Installer"
-
-# Download and uncompress installer archive
-wget https://download.jetbrains.com/python/pycharm-community-2017.1.4.tar.gz
-sudo tar xzvf pycharm-community-2017.1.4.tar.gz -C /opt/
-
-# Install
-cd /opt/pycharm-community-2017.1.4/bin
-echo "Running installer shell script..."
-sudo ./pycharm.sh
+# Download installer archive
+wget https://download.jetbrains.com/python/pycharm-community-2017.2.3.tar.gz
+# Uncompress installer
+sudo tar xzvf pycharm-community-2017.2.3.tar.gz -C /opt/
+# Remove archive
+rm pycharm-community-2017.2.3.tar.gz
+# Link to pycharm script
+sudo ln -s /opt/pycharm-community-2017.2.3/bin/pycharm.sh pycharm
