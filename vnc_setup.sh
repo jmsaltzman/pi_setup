2a25395ba53d6a8e549517f08e7bea17bdcf4d45
J.M. Saltzman <jmsaltzman@gmail.com>
2017-10-27 22:45:06 -0500

    Delete vnc_setup.sh, not needed in latest Raspbian

--- vnc_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -f
-
-# VNC
-# details on tightvncserver setup:
-# http://learn.adafruit.com/adafruit-raspberry-pi-lesson-7-remote-control-with-vnc
-sudo apt-get install -y tightvncserver
-vncserver :1
-mkdir ~/.config/autostart
-cp tightvnc.desktop ~/.config/autostart/
