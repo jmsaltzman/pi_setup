c50a1d6ce6e1c16eef74a0feeb56798f44c0b6b4
Jeff Saltzman <jmsaltzman@gmail.com>
2019-01-08 22:07:43 -0600

    Samba setup tested ok

--- update.sh
+++ update.sh
@@ -3,3 +3,7 @@
 # Update apd database and upgrade installed software
 sudo apt-get -y update
 sudo apt-get -y upgrade
+sudo apt-get -y update
+
+# Install Synaptic for package management
+sudo apt-get install -y synaptic
